684524ecf0fad5663502f112fc099a0d00ff620e
XiaoBoXuan <funnydz@126.com>
2019-06-30 20:27:33 +0800

    query_mac里面分成两个列表 data[0] 查询 eth的mac data[1] 查询 vlan的mac

--- nxapi/query/query_mac.py
+++ nxapi/query/query_mac.py
@@ -4,12 +4,25 @@ import json
 # eth = eth1/5
 # query_mac(serial)
 # query_mac(serial, eth)
+# data[0] -- Ethernet
+# data[1] -- Vlan
 def query_onemac(serial, *eth):
     if len(eth) == 0:
         cli = "show int mac"
     else:
-        cli = "show int " + eth + " mac"
+        cli = "show int " + eth[0] + " mac"
     query = cli_base(serial, cli)
     response = json.loads(query.send().text)
     onemac = response['ins_api']['outputs']['output']['body']['TABLE_interface']['ROW_interface']
-    return onemac
+    EthernetLi = []
+    VlanLi = []
+    for s in onemac:
+        str = s['interface']
+        if str[0] == 'E' and str[1] == 't' and str[2] == 'h':
+            EthernetLi.append(s)
+        elif str[0] == 'V' and str[1] == 'l' and str[2] == 'a':
+            VlanLi.append(s)
+    li = []
+    li.append(EthernetLi)
+    li.append(VlanLi)
+    return li
